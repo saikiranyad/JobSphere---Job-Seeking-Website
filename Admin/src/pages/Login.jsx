387f77d1acf8b7cb6e5f1d06a5b306ad8bf4df95
saikiranyad <saisaikiran7890@gmail.com>
2025-02-06 19:49:55 +0530

    Update Login.jsx

--- Admin/src/pages/Login.jsx
+++ Admin/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = ({ setToken }) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post("http://localhost:3000/api/v1/user/adminlogin", {
+            const response = await axios.post("https://jobsphere-backend-56rb.onrender.com/api/v1/user/adminlogin", {
                 email,
                 password,
             });

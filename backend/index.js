b18af536d9ffad224aa23f275ffad840bab739ac
saikiranyad <saisaikiran7890@gmail.com>
2025-02-06 19:43:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:['http://localhost:5173', 'http://localhost:5179'],
-    credentials:true
-}
+// const corsOptions = {
+//     origin:['http://localhost:5173', 'http://localhost:5179'],
+//     credentials:true
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
